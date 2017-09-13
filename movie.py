bb06367cf760768fb46620c76c57eca2d004a262
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-13 13:52:30 -0400

    Updated from Brython Server: 9/13/2017 1:52:30 PM

--- movie.py
+++ movie.py
@@ -1,1 +1,13 @@
+#Diego Aspinwall
+#9-13-17
+#movie.py
 
+age = int(input('Enter your age: '))
+if age<8:
+    print('You can watch G movies')
+elif 13>age>=8:
+    print('You can watch PG movies')
+elif 18>age>=13:
+    print('You can watch PG-13 movies')
+else:
+    print('You can watch R movies')
