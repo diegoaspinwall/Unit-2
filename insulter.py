0611f82fe0cfdb1a954db39dcc49abed9de0a6ce
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-14 14:41:04 -0400

    Updated from Brython Server: 9/14/2017 2:40:57 PM

--- insulter.py
+++ insulter.py
@@ -1,1 +1,20 @@
+#Diego Aspinwall
+#9-14-17
+#insulter.py - get roasted by a computer that is smarter than you
 
+from random import randint
+
+num = randint(1,5)
+
+name = input('Enter your name: ')
+
+if num==1:
+    print('I once owned a dog that was smarter than you')
+elif num==2:
+    print('If you looked up', name, 'in the dictionary, it would show up under dumb')
+elif num==3:
+    print("Why don't you check ebay and see if they have a life for sale?")
+elif num==4:
+    print('Clay can think of insults faster than you')
+elif num==5:
+    print('I have more of a life than you')
