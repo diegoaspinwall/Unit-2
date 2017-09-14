3a99d89f1dd9a94243a1496293ec9e59ac9f9309
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-14 15:09:36 -0400

    Updated from Brython Server: 9/14/2017 3:09:35 PM

--- adventure.py
+++ adventure.py
@@ -9,5 +9,16 @@ if choi1=='yes':
     print('Thankfully you know how to swim')
     print('However, you notice a nearby shark')
     choi1a = input('Do you punch it on the nose?')
-if choi1a=='yes':
-    print('The shark eats you and you die. LOOOOOLLL you dead bruh')
+    if choi1a=='yes':
+        print('The shark eats your hand and you die. LOOOOOLLL you dead bruh')
+    if choi1a=='no':
+        print('The shark eats you for not defending yourself. It is a very judgemental shark.')
+elif choi1=='no':
+    print('You walk along the bank until you get to the bridge')
+    choi1b = input('Do you cross the bridge to get to the door marked "EXIT"?')
+    if choi1b=='yes':
+        print('You get to the door and find that it is locked. A tiger attacks you.')
+    if choi1b=='no':
+        print('A tiger jumps out of a tree and attacks you. You lose.')
+        
+    
