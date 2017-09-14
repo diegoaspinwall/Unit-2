1da3435ed67ca255571f188baed71afea184846a
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-14 14:55:01 -0400

    Updated from Brython Server: 9/14/2017 2:55:00 PM

--- adventure.py
+++ adventure.py
@@ -1,1 +1,13 @@
+#Diego Aspinwall
+#9-14-17
+#adventure.py
 
+print('A bear is chasing you')
+print('You come to a river')
+choi1 = input('Do you jump in the river?')
+if choi1=='yes':
+    print('Thankfully you know how to swim')
+    print('However, you notice a nearby shark')
+    choi1a = input('Do you punch it on the nose?')
+if choi1a=='yes':
+    print('The shark eats you and you die. LOOOOOLLL you dead bruh')
