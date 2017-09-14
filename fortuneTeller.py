67b6e941862faa0b26b5404208db2e2d1a3c46e7
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-14 14:30:40 -0400

    Updated from Brython Server: 9/14/2017 2:30:39 PM

--- fortuneTeller.py
+++ fortuneTeller.py
@@ -2,4 +2,22 @@
 #9-14-17
 #fortuneTeller.py
 
+color = input('Pick red or blue: ')
+num = int(input('Pick a number from 1-4: '))
 
+if color=='blue' and num==1:
+    print('Pedro will chase you with a flaming torch')
+elif color=='blue' and num==2:
+    print('Clay will annoy you till you die')
+elif color=='blue' and num==3:
+    print('Clay will feed you french fries until you are old and wrinkly')
+elif color=='blue' and num==4:
+    print('Greg will sit on you and squish you into a pulp')
+elif color=='red' and num==1:
+    print('You will be roasted by Charlie until you start running away')
+elif color=='red' and num==2:
+    print('You will leave school riding a flaming chariot while screaming "POTATO!"')
+elif color=='red' and num==3:
+    print('Clay will annoy you till you die')
+elif color=='red' and num==4:
+    print('Clay will annoy you till you die')
