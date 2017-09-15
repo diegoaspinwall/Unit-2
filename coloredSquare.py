f96c9ef6c7fd21547d483cd59cc88267fbe64ef8
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-15 14:34:24 -0400

    Updated from Brython Server: 9/15/2017 2:34:25 PM

--- coloredSquare.py
+++ coloredSquare.py
@@ -1,1 +1,37 @@
+#Diego Aspinwall
+#9-15-17
+#coloredSquare.py
 
+from random import randint
+from ggame import *
+
+numcolor = randint(1,4)
+
+if numcolor==1:
+    red = Color(0xff0000, 1)
+    line = LineStyle(3,red)
+    rectangle = RectangleAsset(100,100,line,red)
+    Sprite(rectangle)
+    myApp = App()
+    myApp.run()
+elif numcolor==2:
+    blue = Color(0x0000ff, 1)
+    line = LineStyle(3,blue)
+    rectangle = RectangleAsset(100,100,line,blue)
+    Sprite(rectangle)
+    myApp = App()
+    myApp.run()
+elif numcolor==3:
+    yellow = Color(0xffff00, 1)
+    line = LineStyle(3,yellow)
+    rectangle = RectangleAsset(100,100,line,yellow)
+    Sprite(rectangle)
+    myApp = App()
+    myApp.run()
+elif numcolor==4:
+    green = Color(0x00ff00, 1)
+    line = LineStyle(3,green)
+    rectangle = RectangleAsset(100,100,line,green)
+    Sprite(rectangle)
+    myApp = App()
+    myApp.run()
