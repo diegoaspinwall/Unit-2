213053a5f98eb00669d688980ad93276724d89f7
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-15 14:43:33 -0400

    Updated from Brython Server: 9/15/2017 2:43:34 PM

--- lastname.py
+++ lastname.py
@@ -1,1 +1,9 @@
+#Diego Aspinwall
+#9-15-17
+#lastname.py
 
+lastname = input('Enter your last name: ')
+if lastname>='N':
+    print(lastname, 'is in the last half of the alphabet')
+else:
+    print(lastname, 'is in the first half of the alphabet')
