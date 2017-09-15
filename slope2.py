2f5e6abcd413a99143b02896536b74e0734cad03
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-15 14:49:52 -0400

    Updated from Brython Server: 9/15/2017 2:49:53 PM

--- slope2.py
+++ slope2.py
@@ -1,1 +1,15 @@
+#Diego Aspinwall
+#9-15-17
+#slope2.py - improving slope.py
 
+x1 = float(input('x1 = '))
+y1 = float(input('y1 = '))
+x2 = float(input('x2 = '))
+y2 = float(input('y2 = '))
+if x1==x2:
+    print('The slope is undefined')
+    print('The equation of the line is X =', x1)
+else:
+    slope = (y2-y1)/(x2-x1)
+    print('The slope is', slope)
+    print('The equation of the line is Y =', slope, 'X +', (y2 - slope * x2))
