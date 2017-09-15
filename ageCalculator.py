ab1b99e7b76638ed2cc1758772d075c13db85b23
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-15 15:04:18 -0400

    Updated from Brython Server: 9/15/2017 3:04:19 PM

--- ageCalculator.py
+++ ageCalculator.py
@@ -7,3 +7,6 @@ from datetime import date
 year = int(input('Enter the year you were born: '))
 month = int(input('Enter the month you were born (1-12): '))
 day = int(input('Enter the day you were born: '))
+
+if date.today(date)month==month and date.today(date)day==day:
+    print ('Happy Birthday, you', date.today()year-year, 'year old!')
