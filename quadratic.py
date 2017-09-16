7f2589187b878b2e3eac0e620a5e450798666f85
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-15 20:34:07 -0400

    Updated from Brython Server: 9/15/2017 8:34:07 PM

--- quadratic.py
+++ quadratic.py
@@ -9,6 +9,9 @@ c = float(input('c = '))
 print('x =', (-b+(b**2-4*a*c)**(1/2))/2*a, 'or')
 print('x =', (-b-(b**2-4*a*c)**(1/2))/2*a)
 
+if (b**2-4*a*c)<0:
+    print('No real solutions, imaginary solutions above.')
+
 """
 mr.Smedinghoff's answer looked different, but when I tested this it worked. Maybe has something to
 do with i?
