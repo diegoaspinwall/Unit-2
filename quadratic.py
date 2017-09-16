e051185369c0fcfb73514d9e4a4bd494f363b4e8
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-15 20:30:26 -0400

    Updated from Brython Server: 9/15/2017 8:30:22 PM

--- quadratic.py
+++ quadratic.py
@@ -1,1 +1,15 @@
+#Diego Aspinwall
+#9-15-17
+#quadratic.py
 
+print('ax^2 + bx + c = 0')
+a = float(input('a = '))
+b = float(input('b = '))
+c = float(input('c = '))
+print('x =', (-b+(b**2-4*a*c)**(1/2))/2*a, 'or')
+print('x =', (-b-(b**2-4*a*c)**(1/2))/2*a)
+
+"""
+mr.Smedinghoff's answer looked different, but when I tested this it worked. Maybe has something to
+do with i?
+"""
