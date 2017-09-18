5d74407f0ea1fc6f2684b4c360ec40245057ba7b
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-18 14:47:07 -0400

    Updated from Brython Server: 9/18/2017 2:47:08 PM

--- base10.py
+++ base10.py
@@ -2,7 +2,14 @@
 #9-18-17
 #base10.py - base 10 number to hex
 
-base10 = int(input('Enter a base 10 number (0-255): '))
+base10a = int(input('Enter a base 10 number (0-255): '))
+base10b = int(input('Enter a base 10 number (0-255): '))
+base10c = int(input('Enter a base 10 number (0-255): '))
 
-dig1 = base10%16
-dig2 = (base10//16)%16
+dig1a = base10a%16
+dig2a = (base10a//16)%16
+dig1b = base10b%16
+dig2b = (base10b//16)%16
+dig1c = base10c%16
+dig2c = (base10c//16)%16
+
